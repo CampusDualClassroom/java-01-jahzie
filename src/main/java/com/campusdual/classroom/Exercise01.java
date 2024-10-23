348c18b44dfb8b8338ddf7c0b59612620c3662ac
jahzie <jahziel.99gl@gmail.com>
2024-10-23 13:49:30 +0200

    Listo

--- src/main/java/com/campusdual/classroom/Exercise01.java
+++ src/main/java/com/campusdual/classroom/Exercise01.java
@@ -3,6 +3,8 @@ package com.campusdual.classroom;
 public class Exercise01 {
 
     public static void main(String[] args) {
-        //TODO → Write: System.out.println("Hello, World!");
-    }
+        //TODO → Write: System.out.println();
+            System.out.println("Hello, World!");
+
+        }
 }
